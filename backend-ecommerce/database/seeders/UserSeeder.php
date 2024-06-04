784f0d80c69ee785498ee1a36203037ac97ff795
A A S <rizkynhg@gmail.com>
2024-06-04 14:28:16 +0700

    update data seeder

--- backend-ecommerce/database/seeders/UserSeeder.php
+++ backend-ecommerce/database/seeders/UserSeeder.php
@@ -17,8 +17,8 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'Administrator',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('password'),
+            'email' => 'rizkynhg@gmail.com',
+            'password' => Hash::make('rizkyindah12'),
         ]);
     }
 }
