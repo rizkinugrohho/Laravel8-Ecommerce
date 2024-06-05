d70314d2bd450661a35d17236bcd9670c3249358
A A S <rizkynhg@gmail.com>
2024-06-05 11:58:58 +0700

    create Restful api carts

--- backend-ecommerce/routes/api.php
+++ backend-ecommerce/routes/api.php
@@ -202,4 +202,36 @@ Route::prefix('web')->group(function () {
         ],
         ['as' => 'web']
     );
+    //get cart
+    Route::get(
+        '/carts',
+        [App\Http\Controllers\Api\Web\CartController::class, 'index'],
+        ['as' =>
+        'web']
+    );
+    //store cart
+    Route::post(
+        '/carts',
+        [App\Http\Controllers\Api\Web\CartController::class, 'store'],
+        ['as' =>
+        'web']
+    );
+    //get cart price
+    Route::get(
+        '/carts/total_price',
+        [App\Http\Controllers\Api\Web\CartController::class, 'getCartPrice'],
+        ['as' => 'web']
+    );
+    //get cart weight
+    Route::get(
+        '/carts/total_weight',
+        [App\Http\Controllers\Api\Web\CartController::class, 'getCartWeight'],
+        ['as' => 'web']
+    );
+    //remove cart
+    Route::post(
+        '/carts/remove',
+        [App\Http\Controllers\Api\Web\CartController::class, 'removeCart'],
+        ['as' => 'web']
+    );
 });
